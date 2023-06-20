284295a7f34ee399795a54e3585946e667a1aecd
Saqib Ahmed <saqibahmed-colan>
2023-06-20 14:53:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/kioskandroid/MainActivity.kt
@@ -0,0 +1,228 @@
+package com.example.kioskandroid
+
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.admin.DevicePolicyManager
+import android.app.admin.SystemUpdatePolicy
+import android.content.*
+import android.os.BatteryManager
+import android.os.Bundle
+import android.os.UserManager
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kioskandroid.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mAdminComponentName: ComponentName
+    private lateinit var mDevicePolicyManager: DevicePolicyManager
+
+    companion object {
+        const val LOCK_ACTIVITY_KEY = "com.example.kioskandroid.MainActivity"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAdminComponentName = MyDeviceAdminReceiver.getComponentName(this)
+        mDevicePolicyManager =
+            getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+
+        mDevicePolicyManager.removeActiveAdmin(mAdminComponentName) //it will remove the active admin
+
+
+        handleClickListener()
+
+        val isAdmin = isAdmin()
+        if (isAdmin) {
+            Snackbar.make(binding.content, R.string.device_owner, Snackbar.LENGTH_SHORT).show()
+        } else {
+            Snackbar.make(binding.content, R.string.not_device_owner, Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun handleClickListener() {
+        binding.btnStartLockTask.setOnClickListener {
+            setKioskPolicies(true, isAdmin())
+        }
+        binding.btnStopLockTask.setOnClickListener {
+            setKioskPolicies(false, isAdmin())
+            val intent = Intent(applicationContext, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+            intent.putExtra(LOCK_ACTIVITY_KEY, false)
+            startActivity(intent)
+        }
+        binding.btnInstallApp.setOnClickListener {
+            //The purpose of this button is to install any android application that you want in kiosk mode
+//            installApp()
+            Toast.makeText(this, "This button is temporary blocked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isAdmin() = mDevicePolicyManager.isDeviceOwnerApp(packageName)
+
+
+    private fun setKioskPolicies(enable: Boolean, isAdmin: Boolean) {
+        if (isAdmin) {
+            setRestrictions(enable)
+            enableStayOnWhilePluggedIn(enable)
+            setUpdatePolicy(enable)
+            setAsHomeApp(enable)
+            setKeyGuardEnabled(enable)
+        }
+        setLockTask(enable, isAdmin)
+        setImmersiveMode(enable)
+    }
+
+
+    private fun setRestrictions(disallow: Boolean) {
+        setUserRestriction(UserManager.DISALLOW_SAFE_BOOT, disallow)
+        setUserRestriction(UserManager.DISALLOW_FACTORY_RESET, disallow)
+        setUserRestriction(UserManager.DISALLOW_ADD_USER, disallow)
+        setUserRestriction(UserManager.DISALLOW_MOUNT_PHYSICAL_MEDIA, disallow)
+        setUserRestriction(UserManager.DISALLOW_ADJUST_VOLUME, disallow)
+        mDevicePolicyManager.setStatusBarDisabled(mAdminComponentName, disallow)
+    }
+
+    private fun setUserRestriction(restriction: String, disallow: Boolean) = if (disallow) {
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, restriction)
+    } else {
+        mDevicePolicyManager.clearUserRestriction(mAdminComponentName, restriction)
+    }
+    // endregion
+
+    private fun enableStayOnWhilePluggedIn(active: Boolean) = if (active) {
+        mDevicePolicyManager.setGlobalSetting(
+            mAdminComponentName,
+            Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
+            (BatteryManager.BATTERY_PLUGGED_AC
+                    or BatteryManager.BATTERY_PLUGGED_USB
+                    or BatteryManager.BATTERY_PLUGGED_WIRELESS).toString()
+        )
+    } else {
+        mDevicePolicyManager.setGlobalSetting(
+            mAdminComponentName,
+            Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
+            "0"
+        )
+    }
+
+    private fun setLockTask(start: Boolean, isAdmin: Boolean) {
+        if (isAdmin) {
+            mDevicePolicyManager.setLockTaskPackages(
+                mAdminComponentName, if (start) arrayOf(packageName) else arrayOf()
+            )
+        }
+        if (start) {
+            startLockTask()
+        } else {
+            stopLockTask()
+        }
+    }
+
+    private fun setUpdatePolicy(enable: Boolean) {
+        if (enable) {
+            mDevicePolicyManager.setSystemUpdatePolicy(
+                mAdminComponentName,
+                SystemUpdatePolicy.createWindowedInstallPolicy(60, 120)
+            )
+        } else {
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName, null)
+        }
+    }
+
+    private fun setAsHomeApp(enable: Boolean) {
+        if (enable) {
+            val intentFilter = IntentFilter(Intent.ACTION_MAIN).apply {
+                addCategory(Intent.CATEGORY_HOME)
+                addCategory(Intent.CATEGORY_DEFAULT)
+            }
+            mDevicePolicyManager.addPersistentPreferredActivity(
+                mAdminComponentName,
+                intentFilter,
+                ComponentName(packageName, MainActivity::class.java.name)
+            )
+        } else {
+            mDevicePolicyManager.clearPackagePersistentPreferredActivities(
+                mAdminComponentName, packageName
+            )
+        }
+    }
+
+    private fun setKeyGuardEnabled(enable: Boolean) {
+        mDevicePolicyManager.setKeyguardDisabled(mAdminComponentName, !enable)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun setImmersiveMode(enable: Boolean) {
+        if (enable) {
+            val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            window.decorView.systemUiVisibility = flags
+        } else {
+            val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            window.decorView.systemUiVisibility = flags
+        }
+    }
+
+    private fun createIntentSender(
+        context: Context?,
+        sessionId: Int,
+        packageName: String?
+    ): IntentSender {
+        val intent = Intent("INSTALL_COMPLETE")
+        if (packageName != null) {
+            intent.putExtra("PACKAGE_NAME", packageName)
+        }
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            sessionId,
+            intent,
+            FLAG_IMMUTABLE
+        )
+        return pendingIntent.intentSender
+    }
+
+
+//    private fun installApp() {
+//        if (!isAdmin()) {
+//            Snackbar.make(binding.content, R.string.not_device_owner, Snackbar.LENGTH_LONG).show()
+//            return
+//        }
+//        val raw = resources.openRawResource(R.raw.other_app)
+//        val packageInstaller: PackageInstaller = packageManager.packageInstaller
+//        val params = PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL)
+//        params.setAppPackageName("com.mrugas.smallapp")
+//        val sessionId = packageInstaller.createSession(params)
+//        val session = packageInstaller.openSession(sessionId)
+//        val out = session.openWrite("SmallApp", 0, -1)
+//        val buffer = ByteArray(65536)
+//        var c: Int
+//        while (raw.read(buffer).also { c = it } != -1) {
+//            out.write(buffer, 0, c)
+//        }
+//        session.fsync(out)
+//        out.close()
+//        createIntentSender(this, sessionId, packageName).let { intentSender ->
+//            session.commit(intentSender)
+//        }
+//        session.close()
+//    }
+
+
+}
